a06ebd462dd6f58251dfd37c4e9188872e371098
davidliu <davidliu@deviange.net>
2022-10-15 22:48:19 +0900

    WebhookReceiver token validation

--- src/main/kotlin/io/livekit/server/WebhookReceiver.kt
+++ src/main/kotlin/io/livekit/server/WebhookReceiver.kt
@@ -1,7 +1,12 @@
 package io.livekit.server
 
 import com.google.protobuf.util.JsonFormat
+import io.jsonwebtoken.Jwts
 import livekit.LivekitWebhook
+import java.security.MessageDigest
+import java.util.*
+import javax.crypto.spec.SecretKeySpec
+
 
 class WebhookReceiver(
     private val apiKey: String,
@@ -18,7 +23,22 @@ class WebhookReceiver(
         if (!skipAuth) {
             requireNotNull(authHeader) { "Auth header is null!" }
             require(authHeader.isNotBlank()) { "Auth header is blank!" }
-            // TODO
+
+            val claimsJws = Jwts.parserBuilder()
+                .setSigningKey(SecretKeySpec(secret.toByteArray(), "HmacSHA256"))
+                .build()
+                .parseClaimsJws(authHeader)
+
+            if (claimsJws.body["iss"] != apiKey) {
+                throw IllegalArgumentException("Issuer doesn't match the given key!")
+            }
+            val digest = MessageDigest.getInstance("SHA-256")
+            val hashBytes = digest.digest(body.toByteArray())
+            val hash = Base64.getEncoder().encodeToString(hashBytes)
+
+            if (claimsJws.body["sha256"] != hash) {
+                throw IllegalArgumentException("sha256 checksum of body does not match!")
+            }
         }
 
         val builder = LivekitWebhook.WebhookEvent.newBuilder()
--- src/test/kotlin/io/livekit/server/WebhookReceiverTest.kt
+++ src/test/kotlin/io/livekit/server/WebhookReceiverTest.kt
@@ -10,10 +10,15 @@ class WebhookReceiverTest {
         val body =
             """{"event":"room_started", "room":{"sid":"RM_TkVjUvAqgzKz", "name":"mytestroom", "emptyTimeout":300, "creationTime":"1628545903", "turnPassword":"ICkSr2rEeslkN6e9bXL4Ji5zzMD5Z7zzr6ulOaxMj6N", "enabledCodecs":[{"mime":"audio/opus"}, {"mime":"video/VP8"}]}}"""
         val testApiKey = "abcdefg"
-        val testSecret = "abababa"
+        val testSecret = "ababababababababababababababababababababababababababababababa"
+
+        val token = AccessToken(testApiKey, testSecret)
+        token.sha256 = "CoEQz1chqJ9bnZRcORddjplkvpjmPujmLTR42DbefYI="
+        val jwt = token.toJwt()
+
         val receiver = WebhookReceiver(testApiKey, testSecret)
 
-        val event = receiver.receive(body, null, true)
+        val event = receiver.receive(body, jwt)
 
         assertEquals("mytestroom", event.room.name)
         assertEquals("room_started", event.event)
