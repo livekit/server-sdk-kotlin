d5e770ed01ddcbc99e44ec3fe52dcac805eba83f
davidliu <davidliu@deviange.net>
2023-04-06 19:30:37 +0900

    Add CanUpdateOwnMetadata video grant

--- src/main/kotlin/io/livekit/server/VideoGrant.kt
+++ src/main/kotlin/io/livekit/server/VideoGrant.kt
@@ -7,15 +7,13 @@ sealed class VideoGrant(val key: String, val value: Any) {
     fun toPair() = key to value
 }
 
+// Protocol videogrant list can be found at
+// https://github.com/livekit/protocol/blob/main/auth/grants.go
+
 /**
  * permission to create rooms
  */
 class RoomCreate(value: Boolean) : VideoGrant("roomCreate", value)
-
-/**
- * permission to join a room
- */
-class RoomJoin(value: Boolean) : VideoGrant("roomJoin", value)
 
 /**
  * permission to list available rooms
@@ -33,6 +31,11 @@ class RoomRecord(value: Boolean) : VideoGrant("roomRecord", value)
 class RoomAdmin(value: Boolean) : VideoGrant("roomAdmin", value)
 
 /**
+ * permission to join a room
+ */
+class RoomJoin(value: Boolean) : VideoGrant("roomJoin", value)
+
+/**
  * name of the room, required if join or admin is set
  * @see [RoomName]
  */
@@ -43,7 +46,6 @@ class Room(value: String) : VideoGrant("room", value)
  * name of the room, required if join or admin is set
  */
 class RoomName(value: String) : VideoGrant("room", value)
-
 
 /**
  * allow participant to publish tracks
@@ -61,6 +63,16 @@ class CanSubscribe(value: Boolean) : VideoGrant("canSubscribe", value)
 class CanPublishData(value: Boolean) : VideoGrant("canPublishData", value)
 
 /**
+ * allow participant to update its own metadata
+ */
+class CanUpdateOwnMetadata(value: Boolean) : VideoGrant("canUpdateOwnMetadata", value)
+
+/**
+ * permission to manage ingress
+ */
+class IngressAdmin(value: Boolean) : VideoGrant("ingressAdmin", value)
+
+/**
  * hide participant from others (used by recorder)
  */
 class Hidden(value: Boolean) : VideoGrant("hidden", value)
@@ -69,10 +81,3 @@ class Hidden(value: Boolean) : VideoGrant("hidden", value)
  * indicates this participant is recording the room
  */
 class Recorder(value: Boolean) : VideoGrant("recorder", value)
-
-
-/**
- * permission to manage ingress
- */
-class IngressAdmin(value: Boolean) : VideoGrant("ingressAdmin", value)
-
