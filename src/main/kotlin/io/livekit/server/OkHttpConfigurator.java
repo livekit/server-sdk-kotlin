afbd555bceecb216b6492efc27df9be80a27dc2a
davidliu <davidliu@deviange.net>
2023-07-01 01:09:36 +0900

    cleanup and fix build

--- src/main/kotlin/io/livekit/server/OkHttpConfigurator.java
+++ src/main/kotlin/io/livekit/server/OkHttpConfigurator.java
@@ -1,9 +1,7 @@
 package io.livekit.server;
 
-import java.net.UnknownHostException;
-
 import okhttp3.OkHttpClient;
 
 public interface OkHttpConfigurator {
-    void config(OkHttpClient.Builder builder) throws UnknownHostException;
+    void config(OkHttpClient.Builder builder);
 }
