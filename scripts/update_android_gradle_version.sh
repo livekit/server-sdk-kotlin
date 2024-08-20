e4349b13b5f5f09d2088f64f364d99f5f3c3cc1e
davidliu <davidliu@deviange.net>
2024-08-20 14:21:53 +0900

    Fix update gradle version script on linux

--- scripts/update_android_gradle_version.sh
+++ scripts/update_android_gradle_version.sh
@@ -1,4 +1,8 @@
 PACKAGE_VERSION=$(cat ./package.json | jq -r '.version')
 echo "updating gradle version name to $PACKAGE_VERSION"
 # sed command works only on linux based systems as macOS version expects a backup file passed additionally
-sed -i '' -e "/VERSION_NAME=/ s/=.*/=$PACKAGE_VERSION/" ./gradle.properties
+if [ "$(uname)" == "Darwin" ]; then
+  sed -i '' -e "/VERSION_NAME=/ s/=.*/=$PACKAGE_VERSION/" ./gradle.properties
+else
+  sed -i -e "/VERSION_NAME=/ s/=.*/=$PACKAGE_VERSION/" ./gradle.properties
+fi
