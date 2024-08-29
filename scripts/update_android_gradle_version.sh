41472a7d6960c2aea021115a78d469a4500bd46a
davidliu <davidliu@deviange.net>
2024-08-30 01:28:50 +0900

    fix changesets action

--- scripts/update_android_gradle_version.sh
+++ scripts/update_android_gradle_version.sh
@@ -5,7 +5,7 @@ set -x
 PACKAGE_VERSION=$(cat ./package.json | jq -r '.version')
 >&2 echo "updating gradle version name to $PACKAGE_VERSION"
 
-SNAPSHOT_VERSION=$(./ci/increment_semver.sh -p $PACKAGE_VERSION)"-SNAPSHOT"
+SNAPSHOT_VERSION=$(./scripts/increment_semver.sh -p $PACKAGE_VERSION)"-SNAPSHOT"
 >&2 echo "next snapshot version to $SNAPSHOT_VERSION"
 
 # sed command works only on linux based systems as macOS version expects a backup file passed additionally
